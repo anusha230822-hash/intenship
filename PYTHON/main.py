c665a108362b805dbfe2f956d97cee96b097d29f
anusha230822-hash <anusha230822@gmail.com>
2026-08-19 14:44:52 +0530

    Add files via upload

--- PYTHON/main.py
+++ PYTHON/main.py
@@ -1,25 +1,2 @@
-from abc import ABC, abstractmethod
-
-class Account(ABC):
-    @abstractmethod
-    def deposit(self, amount):
-        pass
-
-    @abstractmethod
-    def withdraw(self, amount):
-        pass
-
-class SavingsAccount(Account):
-    def __init__(self):
-        self.balance = 0
-    def deposit(self, amount):
-        self.balance += amount
-    def withdraw(self, amount):
-        if amount <= self.balance:
-            self.balance -= amount
-
-if __name__ == '__main__':
-    acc = SavingsAccount()
-    acc.deposit(100)
-    acc.withdraw(30)
-    print('Balance:', acc.balance)
+from file_io_assignment_runner import run
+run(11, 1)
